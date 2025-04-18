a00fbe4471699d139e2a12ecc8edd3180ffea3fe
yangheng95 <yangheng@m.scnu.edu.cn>
2025-04-18 21:16:26 +0100

    0.2.4alpha

--- omnigenome/__init__.py
+++ omnigenome/__init__.py
@@ -8,7 +8,7 @@
 # Copyright (C) 2019-2024. All Rights Reserved.
 
 __name__ = "OmniGenome"
-__version__ = "0.2.4alpha"
+__version__ = "0.2.4alpha2"
 __author__ = "YANG, HENG"
 __email__ = "yangheng2021@gmail.com"
 __license__ = "MIT"
