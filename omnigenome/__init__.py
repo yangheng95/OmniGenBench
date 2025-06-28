dd802fa651faa85501651b0536102bc5585025e3
Heng Yang <yangheng2021@gmail.com>
2025-06-28 20:14:00 +0100

    update_repo

--- omnigenome/__init__.py
+++ omnigenome/__init__.py
@@ -8,7 +8,7 @@
 # Copyright (C) 2019-2024. All Rights Reserved.
 
 __name__ = "OmniGenome"
-__version__ = "0.2.6alpha2"
+__version__ = "0.2.7alpha"
 __author__ = "YANG, HENG"
 __email__ = "yangheng2021@gmail.com"
 __license__ = "Apache-2.0"
