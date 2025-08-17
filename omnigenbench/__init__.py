82b6f567216416dde391dceeeaaf93be3a7e0855
Heng Yang <yangheng2021@gmail.com>
2025-08-17 20:15:34 +0100

    0.3.9alpha

--- omnigenbench/__init__.py
+++ omnigenbench/__init__.py
@@ -26,7 +26,7 @@ Key API Entries:
 """
 
 __name__ = "omnigenbench"
-__version__ = "0.3.8alpha"
+__version__ = "0.3.9alpha"
 __author__ = "YANG, HENG"
 __email__ = "yangheng2021@gmail.com"
 __license__ = "Apache-2.0"
--- omnigenbench/src/explainability/__init__.py
+++
@@ -1,4 +0,0 @@
-from .visualization_2d.explainer import Visualization2DExplainer
-
-
-__all__ = ["Visualization2DExplainer"]
