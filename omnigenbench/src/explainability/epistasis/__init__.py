658b07cc7aeedd6d270490aa52b8667e763ca36e
Heng Yang <yangheng2021@gmail.com>
2025-08-17 20:17:33 +0100

    0.3.10alpha

---
+++ omnigenbench/src/explainability/epistasis/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# File: __init__py
+# Time: 20:17 17/08/2025
+# Author: YANG, HENG <hy345@exeter.ac.uk> (杨恒)
+# Website: https://yangheng95.github.io
+# GitHub: https://github.com/yangheng95
+# HuggingFace: https://huggingface.co/yangheng
+# Google Scholar: https://scholar.google.com/citations?user=NPq5a_0AAAAJ&hl=en
+# Copyright (C) 2019-2025. All rights reserved.
