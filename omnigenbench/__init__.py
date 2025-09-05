d5a1a2ef934386f0bbb0a2d7d26b2860cba916cc
Heng Yang <yangheng2021@gmail.com>
2025-09-05 01:20:46 +0100

    0.3.12alpha

--- omnigenbench/__init__.py
+++ omnigenbench/__init__.py
@@ -26,7 +26,7 @@ Key API Entries:
 """
 
 __name__ = "omnigenbench"
-__version__ = "0.3.11alpha"
+__version__ = "0.3.11alpha2"
 __author__ = "YANG, HENG"
 __email__ = "yangheng2021@gmail.com"
 __license__ = "Apache-2.0"
---
+++ omnigenbench/src/explainability/attention/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# file: __init__.py
+# time: 01:20 05/09/2025
+# author: YANG, HENG <hy345@exeter.ac.uk> (杨恒)
+# homepage: https://yangheng95.github.io
+# github: https://github.com/yangheng95
+# huggingface: https://huggingface.co/yangheng
+# google scholar: https://scholar.google.com/citations?user=NPq5a_0AAAAJ&hl=en
+# Copyright (C) 2019-2025. All Rights Reserved.
